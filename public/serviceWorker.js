82a1813602a202eafe472ce1b22be8ce1d9544d0
Devon <Dwill629@gmail.com>
2021-05-04 01:19:14 -0400

    added addeventlistener to service worker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -9,3 +9,16 @@ const FILES_TO_CACHE = [
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+});
