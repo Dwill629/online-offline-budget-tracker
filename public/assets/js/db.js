97745107f6a2a918b778793b1ff32a4e3e85ef15
Devon <Dwill629@gmail.com>
2021-05-04 01:08:25 -0400

    added check database function

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -28,3 +28,31 @@ saveRecord = (record) => {
 
   store.add(record);
 };
+
+checkDatabase = () => {
+
+  const transaction = db.transaction("pending", "readonly");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then((response) => response.json())
+      .then(() => {
+
+        const transaction = db.transaction("pending", "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+};
