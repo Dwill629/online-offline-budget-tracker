d8bbd46f707ce5e4fab038407910c741de40c019
Devon <Dwill629@gmail.com>
2021-05-04 01:06:26 -0400

    added check online database to db.js

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -6,3 +6,12 @@ request.onupgradeneeded = (event) => {
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
+
+request.onsuccess = (event) => {
+
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
