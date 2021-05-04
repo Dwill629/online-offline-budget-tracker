eb87ffc60727db25499a7a8f1996d3905bd5c3ae
Devon <Dwill629@gmail.com>
2021-05-04 00:58:42 -0400

    updatedfuction to populate table row

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -23,3 +23,19 @@ function populateTotal() {
   let totalEl = document.querySelector("#total");
   totalEl.textContent = total;
 }
+
+function populateTable() {
+  let tbody = document.querySelector("#tbody");
+  tbody.innerHTML = "";
+
+  transactions.forEach(transaction => {
+    // create and populate a table row
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${transaction.name}</td>
+      <td>${transaction.value}</td>
+    `;
+
+    tbody.appendChild(tr);
+  });
+}
