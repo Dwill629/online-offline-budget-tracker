f3c0dc5ded61c8a98b9c85a2ee4fe5b9cdaf5c2a
Devon <Dwill629@gmail.com>
2021-05-14 12:38:31 -0400

    removed Icons block from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,17 +28,7 @@ const config = {
       start_url: "/",
       display: "standalone",
 
-      icons: [
-        {
-          src: path.resolve(
-            __dirname,
-            "public/assets/images/icons/oldIcons/icon-512x512.png"
-            ),
-          // the plugin will generate an image for each size
-          // included in the size array
-          size: [72, 96, 128, 144, 152, 192, 384, 512]
-        }
-      ]
+      
     })
   ]
 };
